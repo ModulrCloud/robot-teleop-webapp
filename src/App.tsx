e7f9cf32499b136b064c10611a2e4dadd3e08294
Michael <michael@modulr.cloud>
2025-10-21 14:27:31 +0100

    Add template Amplify to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 // import { useState } from 'react'
-import './App.css'
-import Navbar from './Navbar';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import ConfirmSignIn from './pages/ConfirmSignIn';
-import EndSession from './pages/EndSession';
-import RobotSelect from './pages/RobotSelect';
-import ServiceSelect from './pages/ServiceSelect';
-import SignIn from './pages/SignIn';
-import Teleop from './pages/Teleop';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import Navbar from "./Navbar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import ConfirmSignIn from "./pages/ConfirmSignIn";
+import EndSession from "./pages/EndSession";
+import RobotSelect from "./pages/RobotSelect";
+import ServiceSelect from "./pages/ServiceSelect";
+import SignIn from "./pages/SignIn";
+import Teleop from "./pages/Teleop";
+import { useEffect, useState } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "../amplify/data/resource";
 
 type TitleUpdaterProps = {
   setIsLoggedIn: (loggedIn: boolean) => void;
@@ -19,42 +26,49 @@ function TitleUpdater({ setIsLoggedIn }: TitleUpdaterProps) {
 
   useEffect(() => {
     switch (location.pathname) {
-      case '/confirm':
+      case "/confirm":
         setIsLoggedIn(false);
-        document.title = 'Confirm Passcode | Modulr';
+        document.title = "Confirm Passcode | Modulr";
         break;
-      case '/signin':
+      case "/signin":
         setIsLoggedIn(false);
-        document.title = 'Sign In | Modulr';
+        document.title = "Sign In | Modulr";
         break;
-      case '/robots':
+      case "/robots":
         setIsLoggedIn(true);
-        document.title = 'Robots | Modulr';
+        document.title = "Robots | Modulr";
         break;
-      case '/services':
+      case "/services":
         setIsLoggedIn(true);
-        document.title = 'Services | Modulr';
+        document.title = "Services | Modulr";
         break;
-      case '/teleop':
+      case "/teleop":
         setIsLoggedIn(true);
-        document.title = 'Teleop | Modulr';
+        document.title = "Teleop | Modulr";
         break;
-      case '/endsession':
+      case "/endsession":
         setIsLoggedIn(false);
-        document.title = 'End Session | Modulr';
+        document.title = "End Session | Modulr";
         break;
       default:
         setIsLoggedIn(false);
-        document.title = 'Home | Modulr';
+        document.title = "Home | Modulr";
     }
   }, [location.pathname]);
 
   return null;
 }
 
+const client = generateClient<Schema>();
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      // next: (data) => setTodos([...data.items]),
+    });
+  }, []);
   // const [count, setCount] = useState(0)
   // const {_, loadedModule} = useCapability({
   //   capability: '@transitive-robotics/remote-teleop',
@@ -62,11 +76,11 @@ function App() {
   //   userId: 'modulr',
   //   deviceId: 'husarion',
   // });
-  
+
   return (
     <Router>
       <TitleUpdater setIsLoggedIn={setIsLoggedIn} />
-      <div className='page-wrapper'>
+      <div className="page-wrapper">
         <Navbar isLoggedIn={isLoggedIn} />
         <main className="main-content">
           <Routes>
@@ -80,9 +94,7 @@ function App() {
         </main>
       </div>
     </Router>
-
-  )
-
+  );
 }
 
-export default App
+export default App;
